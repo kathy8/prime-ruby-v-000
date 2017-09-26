50085a66b815e9acc2d4ae2ad396b6f74e36a721
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2017-09-26 19:19:43 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,21 @@
 def prime?(integer)
     if integer < 2
-        false
+         return false
     elsif integer == 2 || integer == 3
-        true
+      return true
     else
         return prime_detection(integer)
       end
     end
+
+def prime_detection(integer)
+  a = 2
+  while a < integer
+    b = integer /a
+    if a * b == integer
+      false
+    end
+    a += 1
+  end 
+  true
+end 
