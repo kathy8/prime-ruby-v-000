020c2b7e05d1006d477d34e6c3059fe377817712
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2017-09-26 19:12:43 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(integer)
+    if integer <= 1
+        false
+    elsif integer == 2
+        true
+    else 
+        (2..n/2).none? { |i| n % i == 0}
+      end 
+    end 
